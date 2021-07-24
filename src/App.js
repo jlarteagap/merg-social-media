0d50324407ccf41df2f1af2d7cd41778cfdea05f
Jorge <jorgelartearteagap@gmail.com>
2021-07-24 17:36:55 -0400

    routes and semantic ui and nabvar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
 
+import MenuBar from './components/MenuBar'
+import { Container } from 'semantic-ui-react'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div className="App">
-      Hola
-    </div>
+    <Router>
+      <MenuBar />
+      <Container>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+
+    </Router>
   );
 }
 
