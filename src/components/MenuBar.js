ebee6c4dd4bc6d95288e4420bfc707ea0f7f4d81
Jorge <jorgelartearteagap@gmail.com>
2021-07-26 16:44:59 -0400

    token working on localstorage

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,50 +1,56 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Menu } from 'semantic-ui-react'
+import React, { useContext, useState } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
-const MenuBar = () => {
-  const pathname = window.location.pathname
-  const path = pathname === '/' ? 'home' : pathname.substr(1)
-  
-  const [activeItem, setActiveItem] = useState(path)
+import { AuthContext } from '../context/auth';
 
-  const handleItemClick = (e, { name }) => setActiveItem( name )
+function MenuBar() {
+  const { user, logout } = useContext(AuthContext);
+  const pathname = window.location.pathname;
 
-    return (
-        <Menu pointing secondary size="massive" color="orange">
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={handleItemClick}
-            as={Link}
-            to='/'
-          />
-          <Menu.Item
-            name='login'
-            active={activeItem === 'login'}
-            onClick={handleItemClick}
-            as={Link}
-            to='/login'
-          />
-          <Menu.Item
-            name='register'
-            active={activeItem === 'register'}
-            onClick={handleItemClick}
-            as={Link}
-            to='/register'
-          />
-          <Menu.Menu position='right'>
-            <Menu.Item
-              name='logout'
-              active={activeItem === 'logout'}
-              onClick={handleItemClick}
-              as={Link}
-            to='/logout'
-            />
-          </Menu.Menu>
-        </Menu>
-    )
-  }
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
 
+  const handleItemClick = (e, { name }) => setActiveItem(name);
 
-export default MenuBar
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item name={user.username} active as={Link} to="/" />
+
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item
+        name="home"
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="login"
+          active={activeItem === 'login'}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
+        <Menu.Item
+          name="register"
+          active={activeItem === 'register'}
+          onClick={handleItemClick}
+          as={Link}
+          to="/register"
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+
+  return menuBar;
+}
+
+export default MenuBar;
--- src/context/auth.js
+++ src/context/auth.js
@@ -1,4 +1,18 @@
 import { createContext, useReducer } from "react";
+import jwtDecode from 'jwt-decode'
+
+const initialState = {
+    user: null
+}
+if(localStorage.getItem("token")){
+    const decodedToken = jwtDecode(localStorage.getItem("token") )
+    if(decodedToken.exp * 1000 < Date.now()){
+        localStorage.removeItem("token")
+    } else {
+         initialState.user = decodedToken
+    }
+}
+
 const AuthContext = createContext({
     user: null,
     login: (userData) =>{},
@@ -24,9 +38,11 @@ function authReducer(state, action){
 }
 
 function AuthProvider(props){
-    const [state, dispatch] = useReducer(authReducer, { user: null })
+    
+    const [state, dispatch] = useReducer(authReducer, initialState)
 
     const login = (userData) => { 
+        localStorage.setItem("token", userData.token)
         dispatch({
             type: 'LOGIN',
             payload: userData
@@ -34,6 +50,7 @@ function AuthProvider(props){
     }
 
     const logout = () => {
+        localStorage.removeItem("token")
         dispatch({ type: 'LOGOUT'})
     }
 
